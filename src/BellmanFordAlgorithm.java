01fe63448d193bf376008c2d39934daaaf5b6080
yinengy <yinengy@bu.edu>
2018-11-11 00:39:31 -0500

    improve memory

--- src/BellmanFordAlgorithm.java
+++ src/BellmanFordAlgorithm.java
@@ -12,8 +12,10 @@ import java.io.FileNotFoundException;
 public class BellmanFordAlgorithm {
     public static int[][] M; // store Opt(i,v)
 
+    public static int[] M2; // improved memory for current Opt(v)
+
     /**
-     * main algorithm
+     * main algorithm, time: O(mn), Space: O(n^2)
      *
      * @param G a directed graph with no negative cycle in AdjacencyList form
      * @param s the node to start
@@ -34,9 +36,9 @@ public class BellmanFordAlgorithm {
         M[0][t] = 0;
 
         /* For i = 1, . . . , n − 1 */
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i < n; i++) { // O(n)
             /* For v ∈ V in any order */
-            for (int v = 1; v <= n; v++) {
+            for (int v = 1; v <= n; v++) { // check all edges, so totally O(m)
                 /* Compute M[i, v] using the recurrence (6.23) */
                 // compute min w∈V, min(OPT(i − 1, w) + c_vw))
                 int temp = 999; // infinite
@@ -50,7 +52,7 @@ public class BellmanFordAlgorithm {
         } /* Endfor */
 
         /* Return M[n-1, s] */
-        return M[n - 1][s];
+        return M[n - 1][s]; //O(mn)
     }
 
     /**
@@ -58,7 +60,7 @@ public class BellmanFordAlgorithm {
      */
     public static void FindSolution(AdjacencyList G, int s, int t, int[][] c) {
         int n = G.getNumV();
-        System.out.print(s );
+        System.out.print(s);
         for (int i = n - 1; i > 0; i--) {
 
             if (M[i][s] < M[i - 1][s]) {
@@ -72,6 +74,54 @@ public class BellmanFordAlgorithm {
             }
         }
     }
+
+    /**
+     * improved the behavior of memory to O(n)
+     * it is impossible to get the path by backtrack M2
+     * so I will record and print the path in this improved version
+     */
+    public static int ImprovedShortestPath(AdjacencyList G, int s, int t, int[][] c) {
+        int n = G.getNumV();
+
+        M2 = new int[n + 1]; // Space O(n)
+
+        int[] first = new int[n + 1]; // record the path
+
+        for (int v = 1; v <= n; v++) {
+            M2[v] = 999;
+        }
+        M2[t] = 0;
+
+        for (int i = 1; i < n; i++) {
+            for (int v = 1; v <= n; v++) {
+                int temp = 999;
+                int toAdd = 0;
+                for (int w : G.getEdges(v)) {
+                    if (M2[w] + c[v][w] < temp) {
+                        temp = M2[w] + c[v][w];
+                        toAdd = w;
+                    }
+                }
+
+                if (M2[v] > temp) {
+                    M2[v] = temp;
+                    first[v] = toAdd;
+                }
+            }
+        }
+
+        // print the path
+        int temp = s;
+        System.out.print(s );
+        while (temp != t) {
+            temp = first[temp];
+            System.out.print(" -> " + temp);
+        }
+        System.out.println();
+
+        return M2[s];
+    }
+
 
     public static void main(String[] args) throws FileNotFoundException {
         int[][] c = {{0, 1, 2, 3, 4, 5, 6},
@@ -87,8 +137,14 @@ public class BellmanFordAlgorithm {
         AdjacencyList graph = new AdjacencyList(6);
         graph.addFromCSV("test\\DirectedGraphWithNegativeEdge.csv");
         System.out.println(graph);
+        System.out.println("Original version:");
         int cost = ShortestPath(graph, 4, 6, c);
         System.out.println("from " + 4 + " to " + 6 + " cost is " + cost);
         FindSolution(graph, 4, 6, c);
+
+        System.out.println();
+        System.out.println("Memory-improved version:");
+        int cost2 = ImprovedShortestPath(graph, 4, 6, c);
+        System.out.println("from " + 4 + " to " + 6 + " cost is " + cost2);
     }
 }
